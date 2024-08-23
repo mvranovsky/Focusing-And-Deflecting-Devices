45d25ad5de7a9d7ebc982cbc1e0559454e74333a
mvranovsky <vranomic@fjfi.cvut.cz>
2024-08-23 10:18:12 +0200

    7. commit

--- ASTRA/Manual_Example/doubletQ1Q2.py
+++ ASTRA/Manual_Example/doubletQ1Q2.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python3
 
+import numpy as np
+import os
+import re
+import yaml
+import sys
+import subprocess
+import math
+
+
+fileName = "Example"
+fillNumber = "001"
+
+def driftMatrix(length):
+	x = np.array([1, length, 0, 0], [0,1,0,0], [0,0,1,length], [0,0,0,1] )
+	return x
+
+
+def quadrupoleMatrix(K, length):
+	x = np.array([math.cos(math.sqrt(K)*length), math.cos(math.sqrt(K)*length)/K, ])
